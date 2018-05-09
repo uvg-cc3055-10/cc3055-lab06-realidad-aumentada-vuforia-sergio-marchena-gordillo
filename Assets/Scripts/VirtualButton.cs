3f6373e9a900c13db00785c6c78d3b9eab744032
Sergio Marchena <mar16387@uvg.edu.gt>
2018-05-09 09:40:07 -0600

    lab finalizado
    todo funcionando

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private VirtualButtonBehaviour virtualBtn;
+	public Animator birdAnim;
+	//public AudioSource aux;
+	// Use this for initialization
+
+	public void OnButtonPressed (VirtualButtonBehaviour vb) {
+		birdAnim.SetTrigger("sing");
+
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb) {
+
+	}
+
+	void Start () {
+		virtualBtn = GetComponent<VirtualButtonBehaviour>(); 
+		virtualBtn.RegisterEventHandler(this);
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
