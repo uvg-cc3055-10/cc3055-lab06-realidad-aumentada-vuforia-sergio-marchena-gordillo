a300d88b98ae7c8d0ff330f19bf929b258e404c5
sergiomarchena16 <mar16387@uvg.edu.gt>
2018-05-09 10:07:29 -0600

    comments
    comments

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour {
 
+	// variables animacion y de audio
     Animator anim;
     AudioSource audios;
 
 
 	// Use this for initialization
 	void Start () {
+		//animacion y audio
         anim = GetComponent<Animator>();
         audios = GetComponent<AudioSource>();
 	}
@@ -20,6 +22,7 @@ public class Bird : MonoBehaviour {
         anim.SetInteger("hop", 0);
     }
 
+	//ESCUCHAR CANCION
     void PlaySong()
     {
         audios.Play();
