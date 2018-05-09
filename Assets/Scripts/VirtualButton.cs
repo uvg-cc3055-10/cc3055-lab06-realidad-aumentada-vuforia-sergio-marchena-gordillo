c43f35dccf01aace4f62155d44e8447f1b3e4ab0
sergiomarchena16 <mar16387@uvg.edu.gt>
2018-05-09 10:06:02 -0600

    comments
    comments

--- Assets/Scripts/VirtualButton.cs
+++ Assets/Scripts/VirtualButton.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
+//IMPLEMENTACION DEL VIRTUAL BUTTON HANDLER
 public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler {
 
+	//BOTON VIRTUAL Y ANIMATOR
 	private VirtualButtonBehaviour virtualBtn;
 	public Animator birdAnim;
-	//public AudioSource aux;
 	// Use this for initialization
 
+	//METODOS DE LA INTERFACE VIRTUALBUTTONBEHAVIOUR
 	public void OnButtonPressed (VirtualButtonBehaviour vb) {
 		birdAnim.SetTrigger("sing");
 
 	}
-
+	//METODOS DE LA INTERFACE VIRTUALBUTTONBEHAVIOUR
 	public void OnButtonReleased(VirtualButtonBehaviour vb) {
 
 	}
 
 	void Start () {
+		
 		virtualBtn = GetComponent<VirtualButtonBehaviour>(); 
+		//EL HANDLER DE LOS EVENTOS DEL BOTON
 		virtualBtn.RegisterEventHandler(this);
 	}
 
